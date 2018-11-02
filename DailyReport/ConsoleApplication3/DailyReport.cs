f86a2b128c5fe7ca79dd58b4d7b0c83970bcd41c
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-02 10:15:13 -0600

    DailyReport Commit 1

---
+++ DailyReport/ConsoleApplication3/DailyReport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("");
+            Console.WriteLine("What course are you on?");
+            string CurrentCourse = Console.ReadLine();
+            Console.WriteLine("What page number are you currently on in " + CurrentCourse + "?");
+            string CurrentPage = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything?  Please answer \"true\" or \"false\".");
+            string NeedHelp = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string Positives = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide?  Please be specific.");
+            string Feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string StudyHours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers.  An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+           
+        }
+    }
+}
