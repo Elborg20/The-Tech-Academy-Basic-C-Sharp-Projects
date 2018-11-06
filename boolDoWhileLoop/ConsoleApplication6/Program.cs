ccb6255b5f01094bd913f178cacbb31189adbd43
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-06 11:34:25 -0700

    While Loop and a Do While Loop
    Step 94 submission

---
+++ boolDoWhileLoop/ConsoleApplication6/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What are your annual earnings?");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How much money would you like to borrow today?");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int quotient = number1 / number2;
+            bool Larger = (quotient >= 4);
+            do
+            {
+                switch (quotient)
+                {
+                    case 0:
+                        Console.WriteLine("Your numbers do not qualify.");
+                        Console.WriteLine("Please enter a smaller amount for your loan to see if you qualify:");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        quotient = number1 / number2;
+                        break;
+                    case 1:
+                        Console.WriteLine("These numbers do not qualify.");                       
+                        Console.WriteLine("Please enter a smaller amount for your loan to see if you qualify:");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        quotient = number1 / number2;
+                        break;
+                    case 2:
+                        Console.WriteLine("These numbers are getting closer but still do not qualify");                  
+                        Console.WriteLine("Please enter a smaller amount for your loan to see if you qualify:");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        quotient = number1 / number2;
+                        break;
+                    case 3:
+                        Console.WriteLine("Almost.");
+                        Console.WriteLine("Please enter a smaller amount for your loan to see if you qualify: ");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        quotient = number1 / number2;
+                        break;
+
+                    default:
+                        Console.WriteLine("Congratulations.  Based off your input you are qualified to receive a loan of $" + number2 + ".");
+                        Console.ReadLine();
+                        Larger = true;
+                        break;
+                }
+            } while (!Larger);
+             
+            
+        }
+    }
+}
---
+++ boolWhileLoop/ConsoleApplication6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int remainder = number % 7;
+            bool pass = remainder == 2;
+            while (!pass)
+            {
+                switch (remainder)
+                {
+                    case 3:
+                        Console.WriteLine("Almost!");
+                        Console.WriteLine("Guess another number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        remainder = number % 7;
+                        break;
+                    case 1:
+                        Console.WriteLine("Almost!");
+                        Console.WriteLine("Guess another number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        remainder = number % 7;
+                        break;
+                    case 2:
+                        Console.WriteLine("Correct");
+                        pass = true;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect");
+                        Console.WriteLine("Guess another number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        remainder = number % 7;
+                        break;
+                }
+            }
+        }
+    }
+}
