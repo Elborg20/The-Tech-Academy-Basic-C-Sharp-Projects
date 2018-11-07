a192997e0b5e1196f9ca6b61069e7b46108ff16b
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-07 14:45:34 -0700

    Try/Catch step 106 upload

---
+++ TryCatchBlock/ConsoleApplication7/tryCatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> Ages = new List<int>() { 15, 25, 32, 27, 30, 25 };
+                Console.WriteLine("Please pick an integer to divide some numbers by, all quotients will be rounded down");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+                foreach (int age in Ages)
+                {
+                    int quotient = age / divisor;
+                    Console.WriteLine(age + " / " + divisor + " = " + quotient);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter an integer next time.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please pick a number other than zero.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
