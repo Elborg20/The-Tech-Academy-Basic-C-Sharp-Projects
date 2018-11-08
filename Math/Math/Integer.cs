33a81dd6720fef84ebea72bd67530960c3613886
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-08 15:33:33 -0700

    Step 111 submission - Math methods

---
+++ Math/Math/Integer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Math
+{
+    public class Integer
+    {
+        public int firstNumber = 200;
+        public int secondNumber = 15;
+        public int thirdNumber = 20;
+
+        public void Add(int valueOne)
+        {
+            Console.WriteLine(firstNumber + valueOne);
+        }
+        public void Subtract(int valueTwo)
+        {
+            Console.WriteLine(valueTwo - secondNumber);
+        }
+        public void Product(int valueThree)
+        {
+            Console.WriteLine(thirdNumber * valueThree);
+        }
+               
+
+
+    }
+
+
+}
---
+++ Math/Math/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a whole number to add 200 to:");
+            int valueOne = Convert.ToInt32(Console.ReadLine());
+
+            Integer example = new Integer();
+            example.Add(valueOne);
+
+            Console.WriteLine("Pick a whole number to subtract 15 from:");
+            int valueTwo = Convert.ToInt32(Console.ReadLine());
+
+            Integer example2 = new Integer();
+            example2.Subtract(valueTwo);
+
+            Console.WriteLine("Pick a whole number to multiply by 20");
+            int valueThree = Convert.ToInt32(Console.ReadLine());
+
+            Integer example3 = new Integer();
+            example3.Product(valueThree);
+
+            Console.ReadLine();
+
+            
+           
+            
+        }
+
+
+    }
+}
