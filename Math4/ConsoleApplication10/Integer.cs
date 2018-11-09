17f611e0c47c6708358a01db83b2daac830ba3ab
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-09 13:16:54 -0700

    Step 115 submit
    Math methods optional parameter

---
+++ Math4/ConsoleApplication10/Integer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication10
+{
+    public class Integer
+    {
+        int multiply = 3;
+        public void Product (int input1, int input3 = 3)
+        {
+            Console.WriteLine("Your first number * your second number = " + (input1 * input3));
+            Console.WriteLine("If no second number was entered, you will see your first number * 3");
+        }
+      
+    }
+}
---
+++ Math4/ConsoleApplication10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer integer = new Integer();
+            Console.WriteLine("Please enter an integer for program: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another integer for the program if you desire:");
+            string input2 = (Console.ReadLine());
+            if (input2 == string.Empty)
+            {
+                integer.Product(input1);
+            }
+            else
+            {
+                int input3 = Convert.ToInt32(input2);
+                integer.Product(input1, input3);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
