78704d65effa92ead6fc69a929d83d6508444519
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-09 12:47:53 -0700

    Math Operations step 114

---
+++ Math3/ConsoleApplication9/Integer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication9
+{
+    class Integer
+    {
+        public int addend = 20;
+        public double multiplier = 8;
+        public int divisor = 10;
+        public void Operation(int input)
+        {
+            Console.WriteLine("Your number + 20 = " + (input + addend));
+        }
+        public void Operation(double input2)
+        {
+            Console.WriteLine(input2 + " multiplied by 8 and rounded down = " + Convert.ToInt32(multiplier * input2));
+        }
+        
+        public void Operation(string input3)
+        {
+            try
+            {
+                Console.WriteLine("That number divided by 10 and rounded down = " + Convert.ToInt32(input3) / divisor);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You were supposed to enter an integer");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
---
+++ Math3/ConsoleApplication9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Integer integer = new Integer();
+            integer.Operation(input);
+            Console.WriteLine("Please enter a decimal number:");
+            Integer integer2 = new Integer();
+            double input2 = Convert.ToDouble(Console.ReadLine());
+            integer2.Operation(input2);
+            Console.WriteLine("Please enter another number:");
+            Integer integer3 = new Integer();
+            string input3 = Console.ReadLine();
+            integer3.Operation(input3);
+            
+            
+
+
+        }
+    }
+}
