f86a35ecaf028a0a7e2e0db681d2d3bf9a653c68
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-09 12:03:49 -0700

    Math step 113
    Could not figure out how to name my parameters any further

---
+++ Math2/Math2/Integer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Math2
+{
+    public class Integer
+    {
+        public int firstNumber = 50;
+        public void Product( int valueOne, int valueTwo)
+        {
+            Console.WriteLine("The first number times 50 is = " + (valueOne * firstNumber));
+            Console.WriteLine(valueTwo);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Math2/Math2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Math2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer example = new Integer();
+            Console.WriteLine("Please enter an integer for the program");
+            int valueOne = Convert.ToInt32(Console.ReadLine());            
+            Console.WriteLine("Please enter another number for the program:");
+            int valueTwo = Convert.ToInt32(Console.ReadLine());
+            example.Product(valueOne, valueTwo);
+
+        }
+    }
+}
