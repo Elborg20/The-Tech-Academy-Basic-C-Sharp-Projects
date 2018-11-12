52be04f8a719703c704d6fb072f44c2b38ca6e94
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-12 12:49:21 -0700

    step 117 commit

---
+++ Math5/ConsoleApplication9/Integer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication9
+{
+    public class Integer
+    {
+        public void Divide(int input, out int userInputs)
+        {
+            userInputs = 0;
+            Console.WriteLine(input / 2);
+            userInputs++;
+        }
+        public void Divide(int input, int divisor = 2)
+        {
+            Console.WriteLine(input / divisor);
+        }
+    }
+}
---
+++ Math5/ConsoleApplication9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication9
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer integer = new Integer();
+            int userInputs = 0;
+            Console.WriteLine("Please enter an integer:");           
+            int input = Convert.ToInt32(Console.ReadLine());
+            integer.Divide(input, out userInputs);
+            Console.WriteLine(userInputs);
+            integer.Divide(input, 4);
+            Console.ReadLine();
+        }
+
+    }
+}
