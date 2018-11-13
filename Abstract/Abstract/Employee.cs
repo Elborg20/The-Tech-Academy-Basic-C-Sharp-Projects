54a6691533582e77809562b4af8c81505af10e16
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-13 10:32:29 -0700

    step 124 upload

---
+++ Abstract/Abstract/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abstract
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+}
