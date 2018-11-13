44239da12261f89a4587ceb1f5dc6deafaf12fd5
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-13 16:19:35 -0700

    Step 129 submit

---
+++ OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OperatorOverload
+{
+    class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Rate { get; set; }
+
+        public static bool Equals(Employee employee1, Employee employee2)
+        {
+            Console.WriteLine(employee1.EmployeeId == employee2.EmployeeId);
+            return (employee1.EmployeeId==employee2.EmployeeId);
+        }
+    }      
+       
+}
---
+++ OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            employee1.EmployeeId = 105;
+            employee2.EmployeeId = 105;
+            Employee.Equals(employee1, employee2);
+            Console.ReadLine();
+        }
+    }
+}
