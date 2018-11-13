a458661452264ff868aef8565df7bfeec27d07d6
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-13 12:14:52 -0700

    Interfaces step 126 submission

---
+++ Interfaces/Abstract/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abstract
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " has left the building.");
+        }
+    }
+}
