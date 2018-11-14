ef735fdaf8d5b5e1e385d26858e8bb532d5c76fb
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-14 13:52:27 -0700

    Enums step 134

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Enums
+{
+    class Program
+    {
+        enum Days
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+        static void Main()
+        {
+            Console.WriteLine("Please enter the current day of the week.");
+            string input = Console.ReadLine();
+            Days day;
+            try
+            {
+                day = (Days)Enum.Parse(typeof(Days), input);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter the actual day of the week:");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
