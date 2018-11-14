247562a6cebaa11269af7f5bd595dac11c0373a2
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-13 20:07:12 -0700

    step 131

---
+++ Generic/Generic/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generic
+{
+    public class Employee <T>
+    {
+        public List<T> Things { get; set; }
+
+        public void Print(List<T> Things)           
+        {
+            foreach (T thing in Things)
+            {
+                Console.WriteLine(thing);
+               
+            }
+            
+        }
+        
+        
+        
+    }
+
+
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,31 @@
+﻿using Generic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string> { "Joe", "John" };
+            Employee<int> employee1 = new Employee<int>();
+            employee1.Things = new List<int> { 1, 2, 3, 4 };
+            employee.Print(employee.Things);
+            employee1.Print(employee1.Things);
+            
+            Console.ReadLine();
+            
+
+                        
+
+
+
+
+        }
+    }
+}
