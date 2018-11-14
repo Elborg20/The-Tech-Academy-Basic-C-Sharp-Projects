9d28caa790c5d76e3006823d41ccc8e896a486fd
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-14 16:05:53 -0700

    Structs - step 137 commit

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 15;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
