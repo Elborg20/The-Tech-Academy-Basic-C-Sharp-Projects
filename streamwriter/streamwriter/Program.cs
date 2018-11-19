057b37365cae928625b12d50b815877462b6ef92
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-19 09:57:04 -0700

    step 154 submit

---
+++ streamwriter/streamwriter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace streamwriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number for use in the program:");
+            string input = Console.ReadLine();
+            File.WriteAllText(@"C:\\Users\\mtgro\\Logs\\log2.txt", input);
+            string file = File.ReadAllText(@"C:\\Users\\mtgro\\Logs\\log2.txt");
+            Console.WriteLine(file);
+        }
+    }
+}
