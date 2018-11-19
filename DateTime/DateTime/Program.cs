683a7a70a45996eeaf8ded8cf037e4785b054606
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-19 10:37:45 -0700

    step 156 commit

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Now = DateTime.Now;
+            Console.WriteLine(Now);          
+            DateTime twoHoursLater = Now.AddHours(2);
+            Console.WriteLine(twoHoursLater);
+            Console.ReadLine();
+            
+        }
+
+
+    }
+}
