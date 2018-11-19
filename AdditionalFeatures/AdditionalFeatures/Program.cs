211f2174a9e9d463da903fdf9fada8872792b97c
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-19 15:32:27 -0700

    step 162 upload

---
+++ AdditionalFeatures/AdditionalFeatures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace AdditionalFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Matthew Groble";
+            Console.WriteLine("Hello {0}. Welcome to the program.", name);
+            var student = new Construct(name);            
+        }
+    }
+}
---
+++ AdditionalFeatures/AdditionalFeatures/construct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdditionalFeatures
+{
+    class Construct
+    {
+        public Construct (string name) : this(name, 100)
+        {
+        }
+        public Construct (string name, int money)
+        {
+            Name = name;
+            Money = money;
+        }
+        public string Name { get; set; }
+        public int Money { get; set; }
+    }
+}
