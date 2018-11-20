b1cb3f0c9c03de45d99245fd84cb003d2a01ffd3
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-11-19 19:38:34 -0700

    step 165 commit

---
+++ UserDOB/UserDOB/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDOB
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {      
+            try
+            {
+            Console.WriteLine("Please enter your age in integer format, round up if you have not celebrated your birthday this calendar year.");
+            int age = int.Parse(Console.ReadLine());
+            while (age <= 0)
+            {
+                Console.WriteLine("Age must be greater than 0.  Please enter an integer greater than 0");
+                age = int.Parse(Console.ReadLine());
+            }           
+            int currentYear = 2018;           
+            Console.WriteLine(currentYear - age);
+            Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an integer value greater than 0.");
+                int currentyear = 2018;
+                int newage = int.Parse(Console.ReadLine());
+                Console.WriteLine("You were born in " + (currentyear - newage));
+                Console.ReadLine();
+            }
+        }
+    }
+}
